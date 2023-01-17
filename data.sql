296d0dbdaf3e51d028c18222476c21e7948b081e
root <root@LAPTOP-1Q8MR4GQ.localdomain>
2023-01-17 17:41:56 +0100

    ajout single quote

--- data.sql
+++ data.sql
@@ -28,9 +28,9 @@ INSERT INTO plats(nom, prix, description, photo) VALUES
 ('Ratatouille', 9.99, 'Une savoureuse ratatouille faite avec les légumes frais de notre jardin.', '/ratatouille.png'),
 ('Coq au Vin', 14.99, 'Un classique de la cuisine française : le Coq au Vin.', '/coq-au-vin.png'),
 ('Croque-Monsieur', 8.99, 'Un croque-monsieur traditionnel accompagné de frites.', '/croque-monsieur.png'),
-('Crêpes Suzette', 7.99, 'Des crêpes Suzette flambées à la liqueur d\'orange.', '/crepes-suzette.png'),
+('Crêpes Suzette', 7.99, 'Des crêpes Suzette flambées à la liqueur d''orange.', '/crepes-suzette.png'),
 ('Quiche Lorraine', 11.99, 'Une quiche Lorraine traditionnelle avec des lardons et des oignons.', '/quiche-lorraine.png'),
-('Soupe à l\'oignon', 7.99, 'Une soupe à l\'oignon traditionnelle accompagnée de croûtons.', '/soupe-a-l-oignon.png'),
+('Soupe à l''oignon', 7.99, 'Une soupe à l''oignon traditionnelle accompagnée de croûtons.', '/soupe-a-l-oignon.png'),
 ('Steak frites', 16.99, 'Un steak de bœuf grillé accompagné de frites fraîches.', '/steak-frites.png'),
 ('Salade niçoise', 12.99, 'Une salade niçoise avec des légumes frais, des olives et des anchois.', '/salade-nicoise.png'),
 ('Bouillabaisse', 18.99, 'Une bouillabaisse traditionnelle avec des poissons frais de la mer.', '/bouillabaisse.png'),
@@ -70,7 +70,7 @@ INSERT INTO ville(nomVille, codePostal) VALUES
 
 INSERT INTO restaurants(nom, adresse,tempFerme, fraisCharge, idVille) VALUES
 ('À la bonne fourchette !', '13 rue du chat noir', false, 10.45, 1),
-('O\'tacos', '70 place de la République', false, 1.11, 5),
+('O''tacos', '70 place de la République', false, 1.11, 5),
 ('Le Bistrot des Amis', '3 avenue des Champs-Élysées', true, 15.50, 3),
 ('Chez Pierre', '5 rue des Fleurs', false, 12.00, 2),
 ('La Table du Marché', '22 rue de la Boulangerie', false, 18.75, 4),
@@ -141,21 +141,21 @@ INSERT INTO carte_bancaire(titulaire, pays, numero, date, crypto, idClient)
 
 
 INSERT INTO livreurs(prenom, nom, telPro, passwd, enService)
-("Dupont", "Jean", "0612345678", "f5e4d3c2b1a0f6e5d4c3b2a1f7e6d5c4b3a2f1e0d9c8b7a6", true),
-("Martin", "Paul", "0623456789", "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4", false),
-("Durand", "Claire", "0634567890", "g4h3i2j1g5h4i3j2g6h5i4j3g7h6i5j4g8h7i6j5", true),
-("Bertrand", "Luc", "0645678901", "j8i7h6g5j9i8h7g6j0i9h8g7j1i0h9g8j2i1h0g9", false),
-("Lopez", "Sophie", "0601234567", "k7j6i5h4k8j7i6h5k9j8i7h6k0j9i8h7k1j0i9h8", true),
-("Robert", "Julien", "0698765432", "h3g2f1h4g3f2h5g4f3h6g5f4h7g6f5h8g7f6", false),
-("Petit", "Chloe", "0613579864", "f9e8d7f0e9d8f1e0d9f2e1d0f3e2d1f4e3d2", true),
-("Durand", "Antoine", "0624681012", "d5c4b3d6c5b4d7c6b5d8c7b6d9c8b7d0c9b8", false),
-("Leclerc", "Emilie", "0635792468", "b1a0b2a1b3a2b4a3b5a4b6a5b7a6b8a7b9a8", true),
-("Dupont", "Thomas", "0646813579", "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4", false),
-("Garcia", "Michel", "0612347856", "f5e4d3c2b1a0f6e5d4c3b2a1f7e6d5c4b3a2f1e0d9c8b7a6", true),
-("Ruiz", "Nathalie", "0623456789", "a8b7c6d5e4f3a2b1c0d9e8f7g6h5i4j3k2l1a0b9c8d7e6f5", false),
-("Fernandez", "Olivier", "0634567890", "g4h3i2j1g5h4i3j2g6h5i4j3g7h6i5j4g8h7i6j5", true),
-("Miller", "Pascale", "0645678901", "j8i7h6g5j9i8h7g6j0i9h8g7j1i0h9g8j2i1h0g9", false),
-("Rodriguez", "Sebastien", "0613579864", "f9e8d7f0e9d8f1e0d9f2e1d0f3e2d1f4e3d2", true);
+('Dupont', 'Jean', '0612345678', 'f5e4d3c2b1a0f6e5d4c3b2a1f7e6d5c4b3a2f1e0d9c8b7a6', true),
+('Martin', 'Paul', '0623456789', 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4', false),
+('Durand', 'Claire', '0634567890', 'g4h3i2j1g5h4i3j2g6h5i4j3g7h6i5j4g8h7i6j5', true),
+('Bertrand', 'Luc', '0645678901', 'j8i7h6g5j9i8h7g6j0i9h8g7j1i0h9g8j2i1h0g9', false),
+('Lopez', 'Sophie', '0601234567', 'k7j6i5h4k8j7i6h5k9j8i7h6k0j9i8h7k1j0i9h8', true),
+('Robert', 'Julien', '0698765432', 'h3g2f1h4g3f2h5g4f3h6g5f4h7g6f5h8g7f6', false),
+('Petit', 'Chloe', '0613579864', 'f9e8d7f0e9d8f1e0d9f2e1d0f3e2d1f4e3d2', true),
+('Durand', 'Antoine', '0624681012', 'd5c4b3d6c5b4d7c6b5d8c7b6d9c8b7d0c9b8', false),
+('Leclerc', 'Emilie', '0635792468', 'b1a0b2a1b3a2b4a3b5a4b6a5b7a6b8a7b9a8', true),
+('Dupont', 'Thomas', '0646813579', 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4', false),
+('Garcia', 'Michel', '0612347856', 'f5e4d3c2b1a0f6e5d4c3b2a1f7e6d5c4b3a2f1e0d9c8b7a6', true),
+('Ruiz', 'Nathalie', '0623456789', 'a8b7c6d5e4f3a2b1c0d9e8f7g6h5i4j3k2l1a0b9c8d7e6f5', false),
+('Fernandez', 'Olivier', '0634567890', 'g4h3i2j1g5h4i3j2g6h5i4j3g7h6i5j4g8h7i6j5', true),
+('Miller', 'Pascale', '0645678901', 'j8i7h6g5j9i8h7g6j0i9h8g7j1i0h9g8j2i1h0g9', false),
+('Rodriguez', 'Sebastien', '0613579864', 'f9e8d7f0e9d8f1e0d9f2e1d0f3e2d1f4e3d2', true);
 
 
 
