b0333913bb30a3c217fd035242e33d3e8e2c86d5
root <root@LAPTOP-1Q8MR4GQ.localdomain>
2023-01-17 16:38:32 +0100

    ajout data

---
+++ data.sql
@@ -0,0 +1,40 @@
+-- LES STATUS :
+INSERT INTO status(description) VALUES
+('En attente'),
+('En préparation'),
+('En cours de livraison'),
+('Livré'),
+('Annulé');
+
+
+INSERT INTO mots_clefs(mot) VALUES
+('Sushi'),
+('France'),
+('Japon'),
+('Fast-Food'),
+('Patisserie'),
+('Savoyard'),
+('Burger'),
+('Tacos'),
+('Mexicain'),
+('Vegan'),
+('Végétarien'),
+('Gastronomique');
+
+
+INSERT INTO plats(nom, prix, description, photo) VALUES
+('Boeuf-Bourgignon', 12.99, "Un délicieu Boeuf-Bourgignon de nos terroirs français.", "/boeuf-bourgignon.png"),
+('Pizza Margherita', 9.99, "Une pizza à la mozarella qui fera saliver vos papilles.", "/margherita.png"),
+
+
+
+
+
+
+
+
+
+
+
+-- faire des dessins avec les requêtes SQL
+
